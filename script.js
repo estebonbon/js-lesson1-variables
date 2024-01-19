d75ad62d0066977a4990ef0ba804149c4c0afba4
estebonbon <estesarmiento1@gmail.com>
2024-01-18 20:39:32 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Write your JavaScript below this line
+var name = "Esteban";
+var favouriteSnack = "french fries";
+console.log(
+  "Hi, my name is " +
+    name +
+    " and I wish I had " +
+    favouriteSnack +
+    " right now."
+);
